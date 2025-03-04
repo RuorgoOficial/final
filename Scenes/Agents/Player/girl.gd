16edbdcda07705960f975b427498ffba049c45cb
RuorgoOficial <148331999+RuorgoOficial@users.noreply.github.com>
2025-03-04 14:24:46 +0100

    Random encounters while walk into world

--- Scenes/Agents/Player/girl.gd
+++ Scenes/Agents/Player/girl.gd
@@ -8,6 +8,8 @@ const JUMP_VELOCITY = 4.5
 @onready var animation_player = $AnimationPlayer;
 @onready var sprite3D = $Sprite3D;
 
+signal on_player_is_moving()
+
 func _ready() -> void:
 	animation_IDLE()
 
@@ -15,9 +17,10 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		animation_JUMP_START()
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -26,34 +29,72 @@ func _physics_process(delta: float) -> void:
 		var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
+			on_player_is_moving.emit()
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
-			animation_WALK()
+			sprite_direction(direction.x)
+			if not is_jumping and not is_attacking:
+				animation_WALK()
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
-			animation_IDLE()
+			if not is_jumping and not is_attacking:
+				animation_IDLE()
 
 		move_and_slide()
-	elif is_fighting_scene:
-		if Input.is_key_pressed(KEY_W):
-			animation_ATACK()
+		
+	if Input.is_key_pressed(KEY_C):
+		animation_ATACK()
 			
 #region Animation Region
 const ANIMATION_IDLE_NAME : String = "IDLE"
 const ANIMATION_ATACK_NAME : String = "ATACK"
 const ANIMATION_WALK_NAME : String = "WALK"
 
+const ANIMATION_JUMP_START_NAME : String = "JUMP_START"
+const ANIMATION_JUMP_NAME : String = "JUMP"
+const ANIMATION_JUMP_END_NAME : String = "JUMP_END"
+
+var is_jumping : bool = false
+var is_attacking: bool = false
+
 func animation_IDLE():
+	is_jumping = false
+	is_attacking = false
 	animation_player.play(ANIMATION_IDLE_NAME)
 	
 func animation_ATACK():
+	is_attacking = true
 	animation_player.play(ANIMATION_ATACK_NAME)
 	
-func animation_WALK():
+func animation_WALK() -> void: 
 	animation_player.play(ANIMATION_WALK_NAME)
+	
+func animation_JUMP_START() -> void: 
+	is_jumping = true
+	animation_player.play(ANIMATION_JUMP_START_NAME)
+	
+func animation_JUMP() -> void: 
+	animation_player.play(ANIMATION_JUMP_NAME)
+	
+func animation_JUMP_END() -> void: 
+	animation_player.play(ANIMATION_JUMP_END_NAME)
+	
+func sprite_direction(direction:int) -> void:
+	if direction > 0:
+		sprite3D.flip_h = false
+	elif direction < 0:
+		sprite3D.flip_h = true
 #endregion
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == ANIMATION_ATACK_NAME:
-		animation_IDLE();
+		animation_IDLE()
+	elif anim_name == ANIMATION_JUMP_START_NAME:
+		animation_JUMP()
+	elif anim_name == ANIMATION_JUMP_NAME:
+		animation_JUMP_END()
+	elif anim_name == ANIMATION_JUMP_END_NAME:
+		animation_IDLE()
+		
+		
